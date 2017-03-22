876873747c01a4a8ae0e35c490226d3e701edfc3
Hebert Porto <hebertporto@gmail.com>
2017-03-22 13:16:54 -0300

    setup basico para node server

--- server/index.js
+++ server/index.js
@@ -1,21 +1,5 @@
-import express from 'express';
-import http from 'http';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
+import app from './app';
 
-const app = express();
-app.server = http.createServer(app);
-
-app.set('port', 3000);
-
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-
-app.route('/')
-   .get((req, res) => {
-    return res.status(200).json({
-      data: 'retorno res'
-    });
-  });
-
-export default app;
+app.listen(app.get('port'), () => {
+  console.log(`app running on ${app.get('port')}`);
+});
